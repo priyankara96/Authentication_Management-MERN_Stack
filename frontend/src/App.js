ceaeddfa53cd9d51a00fd0047f6a1e1a5ea57536
Priyankara Athapaththu <chamidupriyankara.sl@gmail.com>
2022-07-15 15:55:45 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { RequestContextProvider } from "./services/RequestContext";
+import { UserContextProvider } from "./services/UserContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Home1 from "./components/Home1";
+
+
+export default class App extends Component {
+    render() {
+      return (
+        <RequestContextProvider baseURL={"http://localhost:8000/"}>
+          <UserContextProvider>
+            <BrowserRouter>
+
+              <div style={{ backgroundColor: "#ffff", margin: "0" }}>
+
+                    <Route path="/" exact component={Home1} />
+                    
+              </div>
+            </BrowserRouter>
+          </UserContextProvider>
+        </RequestContextProvider>
+      );
+    }
 }
-
-export default App;
+  
