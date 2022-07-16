86b2a235e196ac3c6a6b604b20836ad3b7fe5cc1
Priyankara Athapaththu <chamidupriyankara.sl@gmail.com>
2022-07-16 20:59:04 +0530

    created the Header

--- frontend/src/components/Home1.jsx
+++ frontend/src/components/Home1.jsx
@@ -3,11 +3,7 @@ import React, { Component } from 'react'
 export default class Home1 extends Component {
   render() {
     return (
-      <div>
-        
-        Home1
-
-      </div>
+      <div>Home1</div>
     )
   }
 }
